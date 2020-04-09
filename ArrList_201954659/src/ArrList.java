86cea848d58f864990e66dd75186211316d52c2a
syoppo <58536569+syoppo@users.noreply.github.com>
2020-04-09 21:24:22 +0900

    Create ArrList.java

---
+++ ArrList_201954659/src/ArrList.java
@@ -0,0 +1,59 @@
+import java.util.NoSuchElementException;
+
+public class ArrList <E>{
+	private E a[];
+	
+	private int size;
+	
+	public ArrList() {
+		a = (E[]) new Object[1];
+		size = 0;
+	}
+	
+	public boolean isEmpty() {return size == 0;}
+	
+	public void insertLast(E newItem) {
+		if(size == a.length)
+			resize(2*a.length);
+		a[size++] = newItem;
+	}
+	
+	public void insert(E newItem, int k) {
+		if(size == a.length)
+			resize(2*a.length);
+		for(int i=size-1; i>=k; i--) a[i+1] = a[i];
+		a[k] = newItem;
+		size++;
+	}
+	
+	public E delete(int k) {
+		if(isEmpty()) throw new NoSuchElementException();
+		E item = a[k];
+		for(int i=k; i<size; i++) a[i] = a[i+1];
+		size--;
+		if(size>0 && size==a.length/4)
+			resize(a.length/2);
+		return item;
+	}
+	
+	public E peek(int k) {
+		if(isEmpty()) throw new NoSuchElementException();
+		return a[k];
+	}
+	
+	private void resize(int newSize) {
+		Object[] t = new Object[newSize];
+		for(int i=0; i<size; i++)
+			t[i] = a[i];
+		a=(E[]) t;
+	}
+	
+	public void print() {
+		if(isEmpty())
+			System.out.print("배열이 비어있음.");
+		else
+			for(int i=0; i<a.length; i++)
+				System.out.print(a[i]+"\t");
+		System.out.println();
+	}
+}
