d0d95266c664bbf9e4c35c64f70368651e252dbc
syoppo <58536569+syoppo@users.noreply.github.com>
2020-04-13 22:21:02 +0900

    Create Node.java

---
+++ Week3-2/SinglyLinkedList/Node.java
@@ -0,0 +1,15 @@
+
+public class Node <E extends Comparable<E>>{
+	private E item;
+	private Node next;
+	
+	public Node(E newItem, Node<E> p) {
+		item = newItem;
+		next = p;
+	}
+	
+	public E getItem() {return item;}
+	public Node getNext() {return next;}
+	public void setItem(E newItem) {item = newItem;}
+	public void setNext(Node n) {next = n;}
+}
