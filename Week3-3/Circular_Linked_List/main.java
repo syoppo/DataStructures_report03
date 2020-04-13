45c37a1f9238a043211349cd6efcb08368182dbb
syoppo <58536569+syoppo@users.noreply.github.com>
2020-04-13 23:00:37 +0900

    Create main.java

---
+++ Week3-3/Circular_Linked_List/main.java
@@ -0,0 +1,18 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CList<String> s = new CList<String>();
+		
+		s.insert("pear"); s.insert("cherry");
+		s.insert("orange"); s.insert("apple");
+		s.print();
+		System.out.print(": s의 길이 = "+s.size()+"\n");
+		
+		s.delete();
+		s.print();
+		System.out.print(": s의 길이 = "+s.size()); System.out.println();
+	}
+
+}
