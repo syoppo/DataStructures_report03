f5e8c8861c275bee3e6cfd02d1fb11257ff89e32
syoppo <58536569+syoppo@users.noreply.github.com>
2020-04-13 21:35:02 +0900

    Create LeeHaeJin_201954659_Sale_2.java

---
+++ Week3-1/List_Example/LeeHaeJin_201954659_Sale_2.java
@@ -0,0 +1,14 @@
+
+public class LeeHaeJin_201954659_Sale_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sale[][] = new int[][] {{63, 84, 140, 130}, {157, 209, 251, 312}};
+		
+		for(int i=0; i<2; i++) {
+			for(int j=0; j<4; j++)
+				System.out.printf("%d/4분기 : sale[%d][%d] = %d %n", j+1, i, j, sale[i][j]);
+		}
+	}
+
+}
